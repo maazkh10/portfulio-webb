d7c300c12fcc4f9c9edd4736c7aae9cbeb0fbb52
maazkh10 <120632580+maazkh10@users.noreply.github.com>
2022-12-25 22:59:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import NAV from "./components/Nav/NAV";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Dark from "./components/Dark mode/dark"
+
+const App = () => {
+  return (
+
+    
+    <>
+    <NAV/>
+     <Header />
+    <dark />
+     
+ <main className='main'>
+  
+ </main>
+    
+    </>
+  )
+}
+export default App
